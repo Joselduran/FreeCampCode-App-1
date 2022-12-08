b2daf0ad85582e403cf85d813d7236eace4d1b2d
Joselduran <joselduran@yahoo.com>
2022-12-08 14:09:15 -0400

    Cambio Fondo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import Logo_fondo_blanco from "./Logo_fondo_blanco.png";
+import Logo_fondo_blanco from "./img/Logo_fondo_blanco.png";
 import "./App.css";
+import js from "./img/js.png";
+import htmlima from "./img/htmlima.png";
+import cssima from "./img/cssima.png";
+import Reactima from "./img/Reactima.png";
 import Boton from "./Componentes/Boton.js";
 import Contador from "./Componentes/Contador.js";
 import { useState } from 'react'
@@ -22,6 +26,12 @@ function App() {
     <div className="App">
       <div className="contenedor-logo">
         <img src={Logo_fondo_blanco} className="josel-logo" alt="logo" />
+      </div>
+      <div className="social">
+        <img src={js} className="prueba" alt="javascript" />
+        <img src={htmlima} className="prueba" alt="html" />
+        <img src={cssima} className="prueba" alt="css" />
+        <img src={Reactima} className="prueba" alt="react" />
       </div>
       <div className="contenedor-contador-botones">
         <Contador numClics={numClics} />
